39710abcaea87b63b435f64c2bfb556aac9b8cb3
James Smith <smithj002@gmail.com>
2013-02-24 13:01:28 +0200

    Update license headers

--- src/upreader.h
+++ src/upreader.h
@@ -1,6 +1,6 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) 2011  Jimi Smith <smithj002@gmail.com>
+    A Google Reader client for Ubuntu Phone
+    Copyright (C) 2013  Jimi Smith <smithj002@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
-
 
 #ifndef UPREADER_H
 #define UPREADER_H
