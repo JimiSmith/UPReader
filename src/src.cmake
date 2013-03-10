2ba456b2c3534b85b50d8498cdb1b7415ae97dca
James Smith <smithj002@gmail.com>
2013-03-10 18:05:07 +0200

    Change the build system slightly

--- src/src.cmake
+++ src/src.cmake
@@ -1,3 +1,25 @@
+find_package(Qt5Core REQUIRED)
+find_package(Qt5Widgets REQUIRED)
+find_package(Qt5Quick REQUIRED)
+find_package(Qt5Xml REQUIRED)
+find_package(Qt5Sql REQUIRED)
+find_package(Qt5Concurrent REQUIRED)
+
+set(CMAKE_AUTOMOC ON)
+  # As moc files are generated in the binary dir, tell CMake
+  # to always look for includes there:
+set(CMAKE_INCLUDE_CURRENT_DIR ON)
+
+add_definitions(-std=c++11)
+
+qt5_add_resources(upreader_image_resources images/images.qrc)
+
+include_directories(
+        ${CMAKE_SOURCE_DIR}
+        ${CMAKE_BINARY_DIR}
+        ${Qt5Quick_INCLUDE_DIRS}
+        ${Qt5Concurrent_INCLUDE_DIRS}
+)
 # We add our source code here
 set(upreader_src src/filteredfeedmodel.cpp
                  src/article.cpp
